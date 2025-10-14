9ff4e7b96f5bb993207932512f5b56da9bada869
teteumachado <trintascripter@gmail.com>
2025-10-14 19:38:46 -0300

    feat: add HasHabilitieMiddleware

--- source/modules/auth/middleware.ts
+++ source/modules/auth/middleware.ts
@@ -1,6 +1,8 @@
 import type { FastifyTypedInstance } from '@/types'
 import { Jwt } from '@/types/jwt'
+import { Habilitie, hasHabilitie, Role } from '@/utils/habilities'
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { UsersService } from '../users/service'
 
 export const AuthMiddleware = (app: FastifyTypedInstance) => {
   return async (request: FastifyRequest, reply: FastifyReply) => {
@@ -20,6 +22,31 @@ export const AuthMiddleware = (app: FastifyTypedInstance) => {
       const decoded = app.jwt.verify(token) as Jwt
 
       request.auth = decoded
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (err) {
+      return reply.status(401).send({ error: 'Unauthorized' })
+    }
+  }
+}
+
+export const HasHabilitieMiddleware = (app: FastifyTypedInstance, habilities: Habilitie[]) => {
+  return async (request: FastifyRequest, reply: FastifyReply) => {
+    const authHeader = request.headers.authorization
+    if (!authHeader) {
+      return reply.status(401).send({ error: 'Authorization header missing' })
+    }
+
+    const [scheme, token] = authHeader.split(' ')
+    if (scheme !== 'Bearer' || !token) {
+      return reply.status(401).send({ error: 'Invalid authorization format' })
+    }
+
+    try {
+      const decoded = app.jwt.verify(token) as Jwt
+      request.auth = decoded
+
+      const userRole = await UsersService.getUserRole(decoded.user.id) as Role
+      if (!hasHabilitie(userRole, habilities)) return reply.status(403).send({ error: 'Forbidden' })
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (err) {
       return reply.status(401).send({ error: 'Unauthorized' })
